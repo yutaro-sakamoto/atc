9791be185868623480fc93f943fa60c51cd3c441
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2024-08-11 16:45:26 +0900

    [Chore]: introduce static analyis tools
    * introduce clang-format and clang-format
    * format  C sources using clang-format
    * add a script running cppcheck
    * add a script running clang-format
    * run cppcheck and clang-format in CI

--- src/atc.c
+++ src/atc.c
@@ -1,15 +1,15 @@
+#include "parser.tab.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "parser.tab.h"
 
 extern int yyparse(void);
 
 int main() {
-    extern FILE *yyin;
-    yyin = stdin;
-    if (yyparse()) {
-        fprintf(stderr, "Parse error!\n");
-        exit(EXIT_FAILURE);
-    }
-    return 0;
+  extern FILE *yyin;
+  yyin = stdin;
+  if (yyparse()) {
+    fprintf(stderr, "Parse error!\n");
+    exit(EXIT_FAILURE);
+  }
+  return 0;
 }
