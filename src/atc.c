d1ccb4274b8c8c9758728f0c4c39d70f1818993c
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2024-08-21 21:18:29 +0900

    [Update]: verify base files

--- src/atc.c
+++ src/atc.c
@@ -6,6 +6,15 @@
 extern int yyparse(void);
 struct atc_list *atc_statement_list;
 
+int verify_base_file(struct atc_list *statement_list) {
+  for(struct atc_list *it = statement_list; it; it = ATC_LIST_NEXT(it)) {
+    if (!IS_ATC_M4_INCLUDE(it->value)) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
 int main(int argc, char *argv[]) {
   extern FILE *yyin;
   char filepath[1024];
@@ -23,26 +32,10 @@ int main(int argc, char *argv[]) {
     exit(EXIT_FAILURE);
   }
   ATC_LIST_REVERSE(atc_statement_list);
-  for (struct atc_list *it = atc_statement_list; it; it = ATC_LIST_NEXT(it)) {
-    struct tree_common *statement = ATC_LIST_VALUE(it);
-    if (IS_ATC_AT_CHECK(statement)) {
-      struct atc_at_check *at_check = ATC_AT_CHECK(statement);
-      printf("AT_CHECK: %s\n", at_check->command);
-    } else if (IS_ATC_AT_SETUP(statement)) {
-      struct atc_at_setup *at_setup = ATC_AT_SETUP(statement);
-      printf("AT_SETUP: %s\n", at_setup->testsuite_name);
-    } else if (IS_ATC_AT_DATA(statement)) {
-      struct atc_at_data *at_data = ATC_AT_DATA(statement);
-      printf("AT_DATA: %s\n", at_data->file_path);
-    } else if (IS_ATC_AT_CLEANUP(statement)) {
-      printf("AT_CLEANUP\n");
-    } else if (IS_ATC_AT_INIT(statement)) {
-      struct atc_at_init *at_init = ATC_AT_INIT(statement);
-      printf("AT_INIT: %s\n", at_init->test_section_name);
-    } else if (IS_ATC_M4_INCLUDE(statement)) {
-      struct atc_m4_include *m4_include = ATC_M4_INCLUDE(statement);
-      printf("M4_INCLUDE: %s\n", m4_include->file_name);
-    }
+
+  if (!verify_base_file(atc_statement_list)) {
+    fprintf(stderr, "Base file must contain only m4_include statements\n");
+    exit(EXIT_FAILURE);
   }
   return 0;
 }
