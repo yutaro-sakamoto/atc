e2aa2f9e46e1a9a48cb713a456f602b9e43993d8
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2024-08-25 22:41:01 +0900

    [Add]: add a function creating a temporary directory

--- src/atc.c
+++ src/atc.c
@@ -2,6 +2,8 @@
 #include "tree.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <dirent.h>
 
 extern int yyparse(void);
 struct atc_list *atc_statement_list;
@@ -20,12 +22,23 @@ int verify_base_file(struct atc_list *statement_list) {
   return 1;
 }
 
-int run_test_file(char* test_name, char* file_name) {
+int create_dir(char *dir) {
+  // TODO: if necessary, dispatch the following process due to the OS
+  return mkdir(dir, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) == 0;
+}
+
+int run_test_file(char *test_name, char *file_name) {
   char filepath[1024];
   sprintf(filepath, "%s.src/%s", test_name, file_name);
   FILE *file = fopen(filepath, "r");
-  if(!file) {
+  if (!file) {
     fprintf(stderr, "Cannot open file %s\n", filepath);
+    return 0;
+  }
+  char dirpath[1024];
+  sprintf(dirpath, "%s.dir", test_name);
+  if(create_dir(dirpath)) {
+    fprintf(stderr, "Cannot create directory %s\n", dirpath);
     return 0;
   }
   return 1;
@@ -54,9 +67,9 @@ int main(int argc, char *argv[]) {
     exit(EXIT_FAILURE);
   }
 
-  struct atc_list* m4_include_files = ATC_LIST_NEXT(atc_statement_list);
-  for(struct atc_list* it = m4_include_files; it; it = ATC_LIST_NEXT(it)) {
-    char* test_file_name = ATC_M4_INCLUDE(ATC_LIST_VALUE(it))->file_name;
+  struct atc_list *m4_include_files = ATC_LIST_NEXT(atc_statement_list);
+  for (struct atc_list *it = m4_include_files; it; it = ATC_LIST_NEXT(it)) {
+    char *test_file_name = ATC_M4_INCLUDE(ATC_LIST_VALUE(it))->file_name;
     if (!run_test_file(argv[1], test_file_name)) {
       exit(EXIT_FAILURE);
     }
