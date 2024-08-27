4d80b74e570283333031fe8c5c7a6f39c082d798
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2024-08-27 20:36:45 +0900

    [WIP]: wip

--- src/atc.c
+++ src/atc.c
@@ -46,6 +46,7 @@ int run_test_file(char *test_name, char *file_name) {
 int main(int argc, char *argv[]) {
   extern FILE *yyin;
   char filepath[1024];
+  struct atc_list *base_file_statement_list;
 
   sprintf(filepath, "%s.at", argv[1]);
   yyin = fopen(filepath, "r");
@@ -59,14 +60,16 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "Parse error!\n");
     exit(EXIT_FAILURE);
   }
-  ATC_LIST_REVERSE(atc_statement_list);
+  base_file_statement_list = atc_statement_list;
 
-  if (!verify_base_file(atc_statement_list)) {
+  ATC_LIST_REVERSE(base_file_statement_list);
+
+  if (!verify_base_file(base_file_statement_list)) {
     fprintf(stderr, "Base file must contain only m4_include statements\n");
     exit(EXIT_FAILURE);
   }
 
-  struct atc_list *m4_include_files = ATC_LIST_NEXT(atc_statement_list);
+  struct atc_list *m4_include_files = ATC_LIST_NEXT(base_file_statement_list);
 
   char dirpath[1024];
   sprintf(dirpath, "%s.dir", argv[1]);
@@ -75,6 +78,24 @@ int main(int argc, char *argv[]) {
     return 0;
   }
 
+  int test_case_count = 0;
+  for (struct atc_list *it = m4_include_files; it; it = ATC_LIST_NEXT(it)) {
+    char filepath[1024];
+    sprintf(filepath, "%s.src/%s", argv[1], ATC_M4_INCLUDE(ATC_LIST_VALUE(it))->file_name);
+    yyin = fopen(filepath, "r");
+    if (!yyin) {
+      fprintf(stderr, "Cannot open file %s\n", filepath);
+      return 0;
+    }
+    if (yyparse()) {
+      fprintf(stderr, "Parse error!: %s\n", filepath);
+      exit(EXIT_FAILURE);
+    }
+    test_case_count += ATC_LIST_LENGTH(atc_statement_list);
+  }
+
+  printf("[dbg] test_case_count: %d\n", test_case_count);
+
   for (struct atc_list *it = m4_include_files; it; it = ATC_LIST_NEXT(it)) {
     char *test_file_name = ATC_M4_INCLUDE(ATC_LIST_VALUE(it))->file_name;
     if (!run_test_file(argv[1], test_file_name)) {
@@ -82,7 +103,7 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  if(remove_dir(dirpath)) {
+  if (remove_dir(dirpath)) {
     fprintf(stderr, "Cannot remove directory %s\n", dirpath);
     return 0;
   }
